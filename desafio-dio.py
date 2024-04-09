d8fbd97150eaf34f3e632e118991256e766a8459
mathcute <104018317+mathcute@users.noreply.github.com>
2024-04-09 12:36:19 -0300

    Update desafio-dio.py

--- desafio-dio.py
+++ desafio-dio.py
@@ -1,4 +1,6 @@
 menu = """
+[u] Cadastrar Usuário
+[c] Criar Conta Bancária
 [d] Depositar
 [s] Sacar
 [e] Extrato
@@ -6,11 +8,41 @@ menu = """
 
 => """
 
+usuarios = {}  # Dicionário para armazenar informações dos usuários
+contas_bancarias = {}  # Dicionário para armazenar informações das contas bancárias
+
 saldo = 0
 limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+
+def cadastrar_usuario(nome, cpf, email):
+    """
+    Cadastra um novo usuário no sistema.
+
+    Args:
+    - nome (str): Nome do usuário.
+    - cpf (str): CPF do usuário.
+    - email (str): E-mail do usuário.
+    """
+    usuarios[cpf] = {'nome': nome, 'email': email}
+    print(f"Usuário {nome} cadastrado com sucesso.")
+
+def criar_conta_bancaria(cpf, saldo_inicial=0, limite=500):
+    """
+    Cria uma nova conta bancária para um usuário.
+
+    Args:
+    - cpf (str): CPF do usuário.
+    - saldo_inicial (float, optional): Saldo inicial da conta. Padrão é 0.
+    - limite (float, optional): Limite da conta. Padrão é 500.
+    """
+    if cpf in usuarios:
+        contas_bancarias[cpf] = {'saldo': saldo_inicial, 'limite': limite}
+        print("Conta bancária criada com sucesso.")
+    else:
+        print("CPF não cadastrado. Por favor, cadastre o usuário primeiro.")
 
 def depositar(valor):
     global saldo, extrato
@@ -26,7 +58,19 @@ def sacar(valor):
 while True:
     opcao = input(menu)
 
-    if opcao == "d":
+    if opcao == "u":
+        nome = input("Nome do usuário: ")
+        cpf = input("CPF do usuário: ")
+        email = input("E-mail do usuário: ")
+        cadastrar_usuario(nome, cpf, email)
+
+    elif opcao == "c":
+        cpf = input("CPF do usuário para criar conta bancária: ")
+        saldo_inicial = float(input("Saldo inicial da conta: "))
+        limite_conta = float(input("Limite da conta: "))
+        criar_conta_bancaria(cpf, saldo_inicial, limite_conta)
+
+    elif opcao == "d":
         valor = float(input("Informe o valor do depósito: "))
         if valor > 0:
             depositar(valor)
@@ -60,3 +104,4 @@ while True:
 
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
+
